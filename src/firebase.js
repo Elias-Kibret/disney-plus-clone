498fccb694d69066c8138958e8e464bf900b8ce3
Elias-Kibret <75451545+Elias-Kibret@users.noreply.github.com>
2021-10-26 01:30:10 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,9 @@ import firebase from "firebase";
 
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
-  // authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
-  // projectId: "disneyplus-clone-a33d5",
-  // storageBucket: "disneyplus-clone-a33d5.appspot.com",
-  // messagingSenderId: "37918794208",
-  // appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
-  // measurementId: "G-DRVLJKWRWG",
 
-  apiKey: "AIzaSyBE5Vuw2Exf-powwbr0-ck74S5rfAin_uU",
+
+
   authDomain: "disney-clone-69c0a.firebaseapp.com",
   projectId: "disney-clone-69c0a",
   storageBucket: "disney-clone-69c0a.appspot.com",
